af6dfc9b50fb086cf9de96660926579fb7a6cb67
kumanoit <amit.kumar.sphere@gmail.com>
2016-12-10 15:51:55 +0530

    Print all possible interpretation of digits on mobile keypad

---
+++ JavaPrograms/src/com/kumanoit/arrays/PrintAllPossibleInterpretationOfDigits.java
@@ -0,0 +1,41 @@
+package com.kumanoit.arrays;
+
+/**
+ * @author kuma wrote on Dec 10, 2016
+ */
+public class PrintAllPossibleInterpretationOfDigits {
+
+	public static void main(String[] args) {
+		int[] digits = {3, 1, 4};
+		String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs",
+				"tuv", "wxyz"};
+		printAllPossibleInterpretation(digits, digits.length, letters, 0,
+				new char[digits.length]);
+	}
+
+	private static void printAllPossibleInterpretation(int[] digits, int n,
+			String[] letters, int level, char[] output) {
+		if (level == n) {
+			for (int i = 0; i < level; i++) {
+				if (output[i] == ' ') {
+					continue;
+				}
+				System.out.print(output[i]);
+			}
+			System.out.println();
+			return;
+		}
+		if (digits[level] == 0 || digits[level] == 1) {
+			output[level] = ' ';
+			printAllPossibleInterpretation(digits, n, letters, level + 1,
+					output);
+		} else {
+			for (int i = 0; i < letters[digits[level]].length(); i++) {
+				output[level] = letters[digits[level]].charAt(i);
+				printAllPossibleInterpretation(digits, n, letters, level + 1,
+						output);
+			}
+		}
+	}
+
+}
